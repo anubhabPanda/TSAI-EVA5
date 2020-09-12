7133c7d7d46ae1af56f37b33dbd1d35b60b09926
anubhabPanda <pandaanubhav@gmail.com>
2020-09-13 03:42:38 +0530

    Removed a folder

--- Picasso/config.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-DATA_DIR = './data'
-DEBUG = False
-BATCH_SIZE = 64
-EPOCHS = 30
-DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
-DATA_DOWNLOAD = True
-NUM_WORKERS = 4
--- Picasso/dataset.py
+++
@@ -1,7 +0,0 @@
-import torchvision.datasets as datasets
-
-def torch_datasets(path, train, download, transforms=None):
-    """
-    Returns a dataset from torchvision library
-    """
-    return  datasets.CIFAR10(path, train, download, transforms)
